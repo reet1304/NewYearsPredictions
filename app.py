95c368a2daa6f2fcc991d3c11d7ec5da6bed900e
Semen Cherezov <semen.nexus@gmail.com>
2020-11-30 16:51:00 +0200

    my site update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 from random import random
 app = Flask(__name__)
 
@@ -9,7 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from' + str(random())
-
+    return 'Hello from' + "sema"
+@app.route('/homepage')
+def oh():
+    return render_template("first.html")
 if __name__ == "__main__":
     print(random())
---
+++ command_examlple.py
@@ -0,0 +1,1 @@
+print('abra cadabra')

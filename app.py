81ce38d6df4c5d5da4ec58268acc51e9a47f6d8d
Semen Cherezov <semen.nexus@gmail.com>
2020-11-30 21:36:02 +0200

    21:35 webhook

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
 from random import random
-app = Flask(__name__)
 
+from bot_token import update_route
+app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello from' + "sema"
+
+
 @app.route('/homepage')
 def oh():
     return render_template("first.html")
+
+
 if __name__ == "__main__":
     print(random())
+
+
+@app.route(update_route)
+def telegram(*args, **kwargs):
+    with open('file.abra.cadabra', 'w') as f:
+        f.write(str(args or "no args"))
